4fcb325f7aafb3b6dfc6f273ae770e7b36ed3b9e
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2025-01-19 22:36:26 +0530

    Created the Hero Banner Section and added styles

---
+++ frontend/src/pages/home/Banner.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import bannerImg from "../../assets/header.png"; 
+const Banner = () => {
+  return (
+    <div className="section__container header__container">
+      <div className="header__content z-30">
+        <h4 className="uppercase">UP TO 20% Discount on</h4>
+        <h1>Girl's Fashion</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam
+          perferendis, aperiam, repellendus unde dolor molestiae vitae porro et
+          tempore, incidunt voluptate consequuntur a consequatur impedit quae
+          ab. Non, quo excepturi?
+        </p>
+        <button className="btn">
+          <Link to="/shop">EXPLORE NOW</Link>
+        </button>
+      </div>
+
+      <div className="header__image">
+        <img src={bannerImg} alt="banner image" />
+      </div>
+    </div>
+  );
+};
+export default Banner;
