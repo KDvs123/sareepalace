cc600d740f15046a3f3bd4de669355d4396a234d
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2025-01-19 22:23:14 +0530

    created the navbar component and add styles to it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-5xl text-red-700 font-serif'>Hello World</h1>
+      <Navbar/>
       <Outlet/>
     </>
   )
