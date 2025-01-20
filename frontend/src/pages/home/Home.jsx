b434ff3f178ec044c0bc147fcc7ce4c839574d27
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2025-01-20 11:08:35 +0530

    created hero content for trends and added styling to it

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import Banner from "./Banner"
 import Categories from "./Categories"
+import HeroSection from "./HeroSection"
 const Home = () => {
   return (
     <>
     <Banner/>
     <Categories/>
+    <HeroSection/>
     </>
   )
 }
