a5ff4320509d170ab212d6c4ff1c372aaba31903
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2025-01-23 21:06:05 +0530

    Install Redux Toolkit and Create Cart Slice

--- frontend/src/pages/shop/ProductCards.jsx
+++ frontend/src/pages/shop/ProductCards.jsx
@@ -1,6 +1,14 @@
 import { Link } from "react-router-dom"
 import RatingStarts from "../../components/RatingStarts"
+import {useDispatch} from "react-redux"
+import { addToCart } from "../../redux/features/cart/cartSlice"
 const ProductCards = ({products}) => {
+
+    const dispatch = useDispatch();
+
+    const handleAddToCart = (product) => {
+        dispatch(addToCart(product))
+    }
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
         {
@@ -15,7 +23,10 @@ const ProductCards = ({products}) => {
                         </Link>
 
                         <div className="hover:block absolute top-3 right-3" >
-                            <button>
+                            <button onClick={(e) => {
+                                e.stopPropagation();
+                                handleAddToCart(product)
+                            }}>
                                <i className="ri-shopping-cart-2-line bg-primary p-1.5 text-white hover:bg-primary-dark"></i> 
                             </button>
                         </div>
