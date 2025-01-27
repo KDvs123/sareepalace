15836d114b5f845e2b6a3cfb4c7816ebe30d0474
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2025-01-27 23:21:16 +0530

    set up mongodb connections

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+
+main().then(()=> console.log("Mongodb is successfully connected")).catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(
+    "mongodb+srv://admin:Saree123@saree.gau58.mongodb.net/?retryWrites=true&w=majority&appName=saree"
+  );
+
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
+}
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
