35ce1b2177e2358b4ebdee2938ab744db8d635fa
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2025-01-27 21:51:40 +0530

    Register Component

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -6,6 +6,7 @@ import Search from "../pages/search/Search";
 import ShopPage from "../pages/shop/ShopPage";
 import SingleProduct from "../pages/shop/productDetails/SingleProduct";
 import Login from "../components/Login";
+import Register from "../components/Register";
 
 const router = createBrowserRouter([
     {
@@ -32,6 +33,12 @@ const router = createBrowserRouter([
         path: "/login",
         element: <Login/>
 
+    }, 
+
+    {
+
+        path: "/register",
+        element: <Register/>
     }
 ]);
 
