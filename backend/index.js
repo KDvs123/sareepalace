08aba8b23619d8c0037652e26fa1b8d8be9264ee
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2025-01-28 20:57:25 +0530

    install dotenv , cors , cookie-parser and body-parser

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,31 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const app = express();
+
 const port = process.env.PORT || 5000;
 
+// middleware steup
+app.use(express.json({limit: "25mb"}));
+app.use(express.urlencoded({limit: "25mb", extended: true}));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true
+  })
+);
 main().then(()=> console.log("Mongodb is successfully connected")).catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect(
-    "mongodb+srv://admin:Saree123@saree.gau58.mongodb.net/?retryWrites=true&w=majority&appName=saree"
+    process.env.DB_URL
   );
 
   app.get("/", (req, res) => {
